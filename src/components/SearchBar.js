0ef9ad4e7d2d6c9e38956d5c581eaeb960cdecfc
Marcello Santoro <marcelloaltermundi@gmail.com>
2022-11-30 15:31:52 -0300

    R10 feat: cria radio buttons

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,10 +3,46 @@ import React, { Component } from 'react';
 class SearchBar extends Component {
   render() {
     return (
-      <input
-        type="text"
-        data-testid="search-input"
-      />
+      <div>
+        <input
+          type="text"
+          data-testid="search-input"
+        />
+        <label htmlFor="Ingredient">
+          Ingredient
+          <input
+            data-testid="ingredient-search-radio"
+            type="radio"
+            name="radioValue"
+            id="Ingredient"
+          />
+        </label>
+        <label htmlFor="Name">
+          Name
+          <input
+            data-testid="name-search-radio"
+            type="radio"
+            name="radioValue"
+            id="Name"
+          />
+        </label>
+        <label htmlFor="First Letter">
+          First Letter
+          <input
+            data-testid="first-letter-search-radio"
+            type="radio"
+            name="radioValue"
+            id="First Letter"
+          />
+        </label>
+        <button
+          data-testid="exec-search-btn"
+          type="button"
+        >
+          Search
+
+        </button>
+      </div>
     );
   }
 }
