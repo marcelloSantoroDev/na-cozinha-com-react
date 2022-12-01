2028633ac1bbeb57cf9e08827cd52df59235bdf6
Marcello Santoro <marcelloaltermundi@gmail.com>
2022-12-01 15:27:51 -0300

    REFACTOR: muda RCC para RFC

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -1,27 +1,32 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
 
-class RecipeCard extends Component {
-  render() {
-    const { thumb, recipeName, id, pathname, index } = this.props;
-    return (
-      <Link
-        to={ `${pathname}/${id}` }
-        data-testid={ `${index}-recipe-card` }
-      >
-        <img
-          src={ thumb }
-          alt={ `${recipeName} img` }
-          width="300px"
-          data-testid={ `${index}-card-img` }
-        />
-        <span data-testid={ `${index}-card-name` }>
-          {recipeName}
-        </span>
-      </Link>
-    );
-  }
+function RecipeCard(props) {
+  const { pathname } = useLocation();
+  const { recipe, index } = props;
+
+  const id = pathname === '/meals' ? recipe.idMeal : recipe.idDrink;
+  const thumb = pathname === '/meals' ? recipe.strMealThumb : recipe.strDrinkThumb;
+  const recipeName = pathname === '/meals' ? recipe.strMeal : recipe.strDrink;
+
+  return (
+    <Link
+      to={ `${pathname}/${id}` }
+      data-testid={ `${index}-recipe-card` }
+    >
+      <img
+        src={ thumb }
+        alt={ `${recipeName} img` }
+        width="300px"
+        data-testid={ `${index}-card-img` }
+      />
+      <span data-testid={ `${index}-card-name` }>
+        {recipeName}
+      </span>
+    </Link>
+  );
 }
 
 RecipeCard.propTypes = {}.isRequired;
+
 export default RecipeCard;
