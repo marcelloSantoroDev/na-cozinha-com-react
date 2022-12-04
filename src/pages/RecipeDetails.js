97c06b8590d060a0d48062f55bb5b2c49a8d21c4
Marcello Santoro <marcelloaltermundi@gmail.com>
2022-12-04 14:39:51 -0300

    R24 feat: finalizado

--- src/pages/RecipeDetails.js
+++ src/pages/RecipeDetails.js
@@ -1,6 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { useLocation, useParams } from 'react-router-dom';
+import { thunkToDrinkDetails, thunkToMealDetails } from '../redux/actions';
 
 function RecipeDetails() {
+  const { id } = useParams();
+  const { pathname } = useLocation();
+  const dispatch = useDispatch();
+
+  console.log(id);
+  console.log(pathname);
+
+  useEffect(() => {
+    if (pathname === `/meals/${id}`) {
+      dispatch(thunkToMealDetails(id));
+    } else {
+      dispatch(thunkToDrinkDetails(id));
+    }
+  }, [dispatch, id, pathname]);
+
   return (
     <div>RecipeDetails</div>
   );
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,6 +1,7 @@
 export const GET_EMAIL = 'GET_EMAIL';
 export const GET_RECIPES = 'GET_RECIPES';
 export const GET_CATEGORIES = 'GET_CATEGORIES';
+export const GET_RECIPE_DETAILS = 'GET_RECIPE_DETAILS';
 
 export const getEmail = (userEmail) => ({
   type: GET_EMAIL,
@@ -15,6 +16,11 @@ export const getRecipes = (recipes) => ({
 export const getCategories = (categories) => ({
   type: GET_CATEGORIES,
   categories,
+});
+
+export const getRecipeDetails = (details) => ({
+  type: GET_RECIPE_DETAILS,
+  details,
 });
 
 export const thunkToRenderMealsRecipes = () => async (dispatch) => {
@@ -78,6 +84,28 @@ export const thunkToFilterDrinksCategories = (category) => async (dispatch) => {
     const request = await fetch(url);
     const { drinks } = await request.json();
     dispatch(getRecipes(drinks));
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+export const thunkToMealDetails = (id) => async (dispatch) => {
+  try {
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
+    const request = await fetch(url);
+    const { meals } = await request.json();
+    dispatch(getRecipeDetails(meals));
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+export const thunkToDrinkDetails = (id) => async (dispatch) => {
+  try {
+    const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`;
+    const request = await fetch(url);
+    const { drinks } = await request.json();
+    dispatch(getRecipeDetails(drinks));
   } catch (error) {
     throw new Error(error);
   }
