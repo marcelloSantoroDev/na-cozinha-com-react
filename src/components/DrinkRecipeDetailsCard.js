82b38d63054447c4a42c07da383e84287c439dfa
Marcello Santoro <marcelloaltermundi@gmail.com>
2022-12-04 17:54:32 -0300

    R25 feat: finalizado

--- src/components/DrinkRecipeDetailsCard.js
+++ src/components/DrinkRecipeDetailsCard.js
@@ -1,12 +1,105 @@
 import React from 'react';
 
 function DrinkRecipeDetailsCard(props) {
-  const { details: { strDrink, strAlcoholic, strDrinkThumb } } = props;
+  const { details } = props;
+  console.log(details);
+  const { strDrink, strAlcoholic, strDrinkThumb, strInstructions, strCategory } = details;
+
+  const { strIngredient1,
+    strIngredient2,
+    strIngredient3,
+    strIngredient4,
+    strIngredient5,
+    strIngredient6,
+    strIngredient7,
+    strIngredient8,
+    strIngredient9,
+    strIngredient10,
+    strIngredient11,
+    strIngredient12,
+    strIngredient13,
+    strIngredient14,
+    strIngredient15,
+  } = details;
+
+  const { strMeasure1,
+    strMeasure2,
+    strMeasure3,
+    strMeasure4,
+    strMeasure5,
+    strMeasure6,
+    strMeasure7,
+    strMeasure8,
+    strMeasure9,
+    strMeasure10,
+    strMeasure11,
+    strMeasure12,
+    strMeasure13,
+    strMeasure14,
+    strMeasure15,
+  } = details;
+
+  const measureArray = [strMeasure1,
+    strMeasure2,
+    strMeasure3,
+    strMeasure4,
+    strMeasure5,
+    strMeasure6,
+    strMeasure7,
+    strMeasure8,
+    strMeasure9,
+    strMeasure10,
+    strMeasure11,
+    strMeasure12,
+    strMeasure13,
+    strMeasure14,
+    strMeasure15];
+
+  const ingredientsArray = [strIngredient1,
+    strIngredient2,
+    strIngredient3,
+    strIngredient4,
+    strIngredient5,
+    strIngredient6,
+    strIngredient7,
+    strIngredient8,
+    strIngredient9,
+    strIngredient10,
+    strIngredient11,
+    strIngredient12,
+    strIngredient13,
+    strIngredient14,
+    strIngredient15];
+
+  const filteredIngredients = ingredientsArray
+    .filter((e) => e !== '' && e !== null && e !== false && e !== ' ');
+
+  const filteredMeasures = measureArray
+    .filter((e) => e !== '' && e !== null && e !== false && e !== ' ');
+
+  const arrayToMap = filteredIngredients.map((e, i) => `${filteredMeasures[i]} of ${e} `);
   return (
     <div className="drink-details-container">
-      <h1>{strDrink}</h1>
-      <img src={ strDrinkThumb } alt={ strDrink } width="200px" />
-      <h2>{strAlcoholic}</h2>
+      <h1 data-testid="recipe-title">{strDrink}</h1>
+      <img
+        data-testid="recipe-photo"
+        src={ strDrinkThumb }
+        alt={ strDrink }
+        width="200px"
+      />
+      <h2 data-testid="recipe-category">{`${strCategory} ${strAlcoholic}`}</h2>
+      <ul>
+        { arrayToMap.map((e, index) => (
+          <li
+            data-testid={ `${index}-ingredient-name-and-measure` }
+            key={ e + index }
+          >
+            {e}
+
+          </li>
+        )) }
+      </ul>
+      <h3 data-testid="instructions">{strInstructions}</h3>
     </div>
   );
 }
