89419823249cc9d5c8d0ab63f000df69439bc01a
Kamilly <kamilly.higinos@gmail.com>
2022-12-08 16:54:14 -0300

    Tela de Perfil feat: R58, R60, R61, R62 (Email visivel e redirecionamento de rotas)

--- src/components/ProfileComponent.js
+++ src/components/ProfileComponent.js
@@ -1,19 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+// import Header from './Header';
+import Footer from './Footer';
 
 function ProfileComponent() {
+  const [emailProfile, setEmailProfile] = useState('');
+  const history = useHistory();
+
+  useEffect(() => {
+    const userEmail = JSON.parse(localStorage.getItem('user'));
+    setEmailProfile(userEmail.email);
+  }, [setEmailProfile]);
   return (
-    <form>
-      <input type="email" name="email" id="email" data-testid="profile-email" />
-      <button type="button" data-testid="profile-done-btn">
+    <div>
+      {/* <Header /> */}
+      <h4 data-testid="profile-email">{emailProfile}</h4>
+      <button
+        type="button"
+        data-testid="profile-done-btn"
+        onClick={ () => history.push('/done-recipes') }
+      >
         Done Recipes
       </button>
-      <button type="button" data-testid="profile-favorite-btn">
+      <button
+        type="button"
+        data-testid="profile-favorite-btn"
+        onClick={ () => history.push('/favorite-recipes') }
+      >
         Favorite Recipes
       </button>
-      <button type="button" data-testid="profile-logout-btn">
+      <button
+        type="button"
+        data-testid="profile-logout-btn"
+        onClick={ () => {
+          localStorage.clear();
+          setEmailProfile('');
+          history.push('/');
+        } }
+      >
         Logout
       </button>
-    </form>
+      <Footer />
+    </div>
   );
 }
 
