795d206152f84ee5a736b46178fa3eccdab69a25
Kamilly <kamilly.higinos@gmail.com>
2022-12-08 14:04:37 -0300

    R57 feat: finalizado

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import ProfileComponent from '../components/ProfileComponent';
 
 function Profile() {
   return (
     <>
-      <Header
-        title="Profile"
-      />
+      <Header title="Profile" />
+      <ProfileComponent />
       <Footer />
     </>
   );
