24c675b37d681b1524866d541efad7ec6489e821
Marcello <marcelloaltermundi@gmail.com>
2022-12-27 19:36:01 -0300

    add dependências (notebook novo)

--- src/pages/FavoriteRecipes.js
+++ src/pages/FavoriteRecipes.js
@@ -1,4 +1,3 @@
-import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import FavoriteRecipesCard from '../components/FavoriteRecipesCard';
 import Header from '../components/Header';
